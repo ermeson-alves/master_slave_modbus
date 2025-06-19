e9df3bbbc4d3d0fb7afb21f6cae6dbf4a581e7e6
Eduardo Monteiro de Sousa <edu123monteiro@gmail.com>
2025-06-19 13:22:40 -0300

    Cria programa OpenPLC principal para mestre

---
+++ master/openplc/build/POUS.c
@@ -0,0 +1,64 @@
+void LOGGER_init__(LOGGER *data__, BOOL retain) {
+  __INIT_VAR(data__->EN,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->ENO,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->TRIG,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->MSG,__STRING_LITERAL(0,""),retain)
+  __INIT_VAR(data__->LEVEL,LOGLEVEL__INFO,retain)
+  __INIT_VAR(data__->TRIG0,__BOOL_LITERAL(FALSE),retain)
+}
+
+// Code part
+void LOGGER_body__(LOGGER *data__) {
+  // Control execution
+  if (!__GET_VAR(data__->EN)) {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(FALSE));
+    return;
+  }
+  else {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(TRUE));
+  }
+  // Initialise TEMP variables
+
+  if ((__GET_VAR(data__->TRIG,) && !(__GET_VAR(data__->TRIG0,)))) {
+    #define GetFbVar(var,...) __GET_VAR(data__->var,__VA_ARGS__)
+    #define SetFbVar(var,val,...) __SET_VAR(data__->,var,__VA_ARGS__,val)
+
+   LogMessage(GetFbVar(LEVEL),(char*)GetFbVar(MSG, .body),GetFbVar(MSG, .len));
+  
+    #undef GetFbVar
+    #undef SetFbVar
+;
+  };
+  __SET_VAR(data__->,TRIG0,,__GET_VAR(data__->TRIG,));
+
+  goto __end;
+
+__end:
+  return;
+} // LOGGER_body__() 
+
+
+
+
+
+void RS485_MASTER_init__(RS485_MASTER *data__, BOOL retain) {
+  __INIT_VAR(data__->START_BUTTON,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->MOTOR_ENABLE,__BOOL_LITERAL(FALSE),retain)
+}
+
+// Code part
+void RS485_MASTER_body__(RS485_MASTER *data__) {
+  // Initialise TEMP variables
+
+  __SET_VAR(data__->,MOTOR_ENABLE,,__GET_VAR(data__->START_BUTTON,));
+
+  goto __end;
+
+__end:
+  return;
+} // RS485_MASTER_body__() 
+
+
+
+
+
---
+++ master/openplc/build/POUS.h
@@ -0,0 +1,46 @@
+#include "beremiz.h"
+#ifndef __POUS_H
+#define __POUS_H
+
+#include "accessor.h"
+#include "iec_std_lib.h"
+
+__DECLARE_ENUMERATED_TYPE(LOGLEVEL,
+  LOGLEVEL__CRITICAL,
+  LOGLEVEL__WARNING,
+  LOGLEVEL__INFO,
+  LOGLEVEL__DEBUG
+)
+// FUNCTION_BLOCK LOGGER
+// Data part
+typedef struct {
+  // FB Interface - IN, OUT, IN_OUT variables
+  __DECLARE_VAR(BOOL,EN)
+  __DECLARE_VAR(BOOL,ENO)
+  __DECLARE_VAR(BOOL,TRIG)
+  __DECLARE_VAR(STRING,MSG)
+  __DECLARE_VAR(LOGLEVEL,LEVEL)
+
+  // FB private variables - TEMP, private and located variables
+  __DECLARE_VAR(BOOL,TRIG0)
+
+} LOGGER;
+
+void LOGGER_init__(LOGGER *data__, BOOL retain);
+// Code part
+void LOGGER_body__(LOGGER *data__);
+// PROGRAM RS485_MASTER
+// Data part
+typedef struct {
+  // PROGRAM Interface - IN, OUT, IN_OUT variables
+  __DECLARE_VAR(BOOL,START_BUTTON)
+  __DECLARE_VAR(BOOL,MOTOR_ENABLE)
+
+  // PROGRAM private variables - TEMP, private and located variables
+
+} RS485_MASTER;
+
+void RS485_MASTER_init__(RS485_MASTER *data__, BOOL retain);
+// Code part
+void RS485_MASTER_body__(RS485_MASTER *data__);
+#endif //__POUS_H
